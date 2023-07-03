c12bb024e375fb53a37cbee1ee1af9ce8aa606f9
Yevhen Barkov <mr.e.barkov@gmail.com>
2023-07-03 09:55:38 +0300

    final commit

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const xhr = new XMLHttpRequest();
   const url =
     "http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=5d066958a60d315387d9492393935c19";
 
-  xhr.open("GET", url, true);
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      const data = JSON.parse(xhr.responseText);
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(
+          `Помилка отримання погодних даних. Статус: ${response.status}`
+        );
+      }
+      return response.json();
+    })
+    .then((data) => {
       const { temp, pressure, description, humidity, wind } = data.main;
       const { speed, deg } = data.wind;
       const icon = data.weather[0].icon;
@@ -23,10 +28,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
       const weatherContainer = document.getElementById("weather");
       weatherContainer.innerHTML = weatherHtml;
-    } else if (xhr.readyState === 4 && xhr.status !== 200) {
-      console.log(`Помилка отримання погодних даних. Статус: ${xhr.status}`);
-    }
-  };
-
-  xhr.send();
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
 });
